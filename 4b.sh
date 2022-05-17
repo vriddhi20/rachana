3b9a5518e047af844177348e2062b4e38eb86a68
thanushags <98949476+thanushags@users.noreply.github.com>
2022-05-17 16:45:19 +0530

    Add files via upload

---
+++ 4b.sh
@@ -0,0 +1,21 @@
+if [ $# -ge 1 ]
+then
+   if [ $# -eq 2 ]
+   then
+	chd=$2
+   else
+	chd=.
+   fi
+   file= `find ~ -iname $1`
+   if [ `echo $file | wc -c` -gt 1 ]
+   then
+     number =`ls -inum $file | grep -0 [0-9]`
+     cd $chd
+     find . -inum $number
+   else
+     echo "No such file"
+  fi
+else
+echo "Enter argument"
+fi
+
